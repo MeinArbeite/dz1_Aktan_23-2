78e1ad4dad4a7ae5030ec3c4243ebb8933f39cac
Актан <kenzhekarievaktan@gmail.com>
2023-02-07 12:51:23 +0600

    dz1

---
+++ Lesson_1/aap.js
@@ -0,0 +1,76 @@
+/*
+Урок 1
+Автор: Евгений Киселев
+
+Группа:23
+Тема урока: основы JavaScript
+*/
+
+var a = 5
+        console.log(a)
+        console.log("Hello Js")
+
+//Я однострочный комментарий аааааааабббббббб
+/*
+Я 
+многострочный 
+комментарий
+*/
+// 1) String текстовый тип данных
+var customerFullName = "Eugene Kiselev" //Camel case
+var customerINN = '12736570'
+
+console.log(typeof "", "completed")
+
+//2) Nummer - числовой
+var customerCurrentBalance = 500
+var customerCurrentBalance2 = -1000.24
+var customerScoring = 3.451518
+console.log(typeof 5, "completed")
+
+// 3)Boolean - логичесикий
+var customerIsResident = true
+var customerUseMobileBanking = false
+console.log(typeof true, "completed")
+
+//4) undefined - неопределенный
+var customerBithDay
+console.log(typeof customerIsResident, "completed")
+
+//5) null - нулевой
+var customerComtactPhone = null
+console.log(typeof "completed") // баг JS
+console.log("null completed")
+
+
+//6) object обьективный (комплексный)
+// var customerPassport = {
+//         series: "AN",
+//         number: 12345,
+//         expiryDate: null,
+//         isUnlimited: true
+// }
+// console.log(typeof {}, "completed")
+//7) Symbol - символьный
+//8) BigInt - длинная арифметика
+
+// var customerCountry = prompt("Type your country")
+// var customerCity = prompt("Type your city")
+// var customerStreet = prompt("Type your street")
+// var customerHouse = prompt("Type your house")
+
+//Concatenation - конкатенация
+// var customerLocation =
+// customerCountry +
+// " "  +
+// customerCity +
+// " "  +
+// customerStreet +
+// " "  +
+// customerHouse
+
+// alert("Your location is :" + customerLocation);
+// console.log("Your location is :" + customerLocation);
+
+
+customerFullName
---
+++ Lesson_1/app.js
@@ -0,0 +1,60 @@
+
+// const Name = prompt("Имя")
+// const surName = prompt("Фамилия")
+
+// alert("Здравствуйте, " + Name +" " + surName)
+
+// 2 Untericht
+
+    // const Svetofor = prompt("Выберите цвет?(green,red,yellow)");
+    // switch (Svetofor) {
+    //     case "yellow":
+    //         console.log("Gehen");
+            
+    //         break;
+    //     case "green":
+    //         console.log("Go");
+    //         break;
+    //     case "red":
+    //         console.log("stop");    
+    //         break;
+    //     default:
+    //             console.log("выберите цвет правильно");
+    //         break;
+    // }
+
+
+// 3 Untericht
+const jome = Number(prompt("choose a number 0-9"))
+switch (jome) {
+    case 1:
+        console.log("I");
+        break;
+    case 2:
+        console.log("II");
+        break;
+    case 3:
+        console.log("III");
+        break;
+    case 4:
+        console.log("IV");
+        break;
+    case 5:
+        console.log("V");
+        break;
+    case 6:
+        console.log("VI");
+        break;
+    case 7:
+        console.log("VII");
+        break;
+    case 8:
+        console.log("VIII");
+        break;
+    case 9:
+        console.log("IX");
+        break;
+    default:
+        console.log("CHOOSE A CORECT NUMBER!!!");
+        break;
+}
